6586f89173325195180e00b007e341cfa52d72eb
GuiTomaz <lhermetomaz@gmail.com>
2020-05-06 12:21:42 -0300

    Calculadora de integrais e derivadas

---
+++ CalculadoraDeIntegral&Derivada.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+int* coletaCoeficiente(int g);
+void calculaIntegral(int g, int* coef);
+void calculaDerivada(int g, int* coef);
+int main(void){
+  //Recebe o polinomio
+  int g; //maior valor do polibomio
+  int* coeficientes;
+  printf("Informe o valor do maior grau do polinomio que quer integrar/derivar: (maior expoente) ");
+  scanf("%d",&g);
+  coeficientes = coletaCoeficiente(g);
+  calculaIntegral(g, coeficientes);
+  calculaDerivada(g, coeficientes);
+  free(coeficientes);
+  return 0;
+}
+int* coletaCoeficiente(int g){
+  int* coef = (int*) malloc(sizeof(int)*g+1);
+  for(int c = 0;c<g+1;c++){
+    printf("\nInforme o coeficiente que acompanha x^%d: ", c);
+    scanf("%d",&coef[c]);
+    printf("\n -------------");
+  }
+  return coef;
+
+}
+void calculaIntegral(int g, int* coef){
+  //Algoritmo para calcular a integral do polinomio dado
+  float integral[g+2];
+  integral[0]=0;//representa a constante 'c';
+  for(int i =0;i<g+1;i++){
+    integral[i+1] =  coef[i] / (i+1)*1.0;
+  }
+
+  //Exibe a Integral
+  printf("\n Integral: ");
+  for(int k = g+1;k>0;k--){
+    printf("(%.0fx^%d) + ",integral[k],k);
+  }
+  printf(" c ");
+}
+
+void calculaDerivada(int g, int* coef){
+  //Algoritmo para calcular a derivada
+  int derivada[g];
+  for(int z = 1;z<g+1;z++){
+    derivada[z-1] = coef[z]*z;
+
+  }
+
+  //Exibe a derivada
+  printf("\n Derivada: ");
+  for(int x = g-1;x>=0;x--){
+    printf("(%dx^%d) + ", derivada[x],x);
+  }
+  printf(" 0 ");
+}
