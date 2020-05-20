ff140fe494a41388b4d272c11a678ab18cfaeed9
GuiTomaz <lhermetomaz@gmail.com>
2020-05-20 13:12:29 -0300

    Correcao da solucao

--- StringContida.c
+++ StringContida.c
@@ -8,6 +8,7 @@ void exibe(char** lista, int qtdFatias);
 void fatiarString(char* str1, char** lista, int qtdFatias, int tamStr2);
 void armazenar(char* aux, char** lista, int indiceAtual);
 void desalocarMemoria(char** memory, int qtdEnd);
+int compara(char* str1, char* str2);
 int main() {
   char str1[30];
   char str2[30];
@@ -36,7 +37,7 @@ int cic(char* str1, char* str2){
   fatiarString(str1, stringFatiada, qtdFatias, tamStr2);
   //Finalmente tenta descobrir se a string2 esta em string1
   for(int i =0;i<qtdFatias;i++){
-    if(strcmp(stringFatiada[i], str2) == 0){
+    if(compara(stringFatiada[i], str2) == 1){
       desalocarMemoria(stringFatiada, qtdFatias);
       return 1;
     }
@@ -44,6 +45,21 @@ int cic(char* str1, char* str2){
   desalocarMemoria(stringFatiada, qtdFatias);
   return 0;
 
+}
+
+int compara(char* str1, char* str2){
+  int i =0;
+  if(strlen(str1) != strlen(str2)){
+    return 0; // se possuem tamanhos diferentes, certamente serao diferentes
+  }
+
+  while(str1[i]!='\0'){
+    if(str1[i] != str2[i] && (str1[i] + 32 != str2[i] && str1[i]-32!=str2[i])){
+      return 0;
+    }
+    i++;
+  }
+  return 1;
 }
 void exibe(char** lista, int qtdFatias){
   for(int i =0;i<qtdFatias;i++){
